62ae962c648651363d1cb1cc83f53c2ac655ddeb
tarikomalasari <tarikomalasari02@gmail.com>
2021-10-24 21:54:27 +0700

    menambahkab index.html,about.html,contact.html

---
+++ application/views/Beranda.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width, initial-scale=1.0">
+	<title>Beranda</title>
+</head>
+<body>
+	<h2>Hai Nama Saya adalah Tari Komalasari</h2>
+
+</body>
+</html>
